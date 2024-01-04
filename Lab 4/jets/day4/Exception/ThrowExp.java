238fa9eba23d69cc64f5bbb322e9c74ddffa6bf7
Steven Adel <111876286+stevenadel@users.noreply.github.com>
2024-01-04 02:08:47 +0200

    Update ThrowExp.java

--- Lab 4/jets/day4/Exception/ThrowExp.java
+++ Lab 4/jets/day4/Exception/ThrowExp.java
@@ -2,14 +2,14 @@ package jets.day4.Exception;
 
 public class ThrowExp {
     public void exceptionThrow1() throws SteveException {
-        throw new SteveException("This is com.jets.day4.exception method 1");
+        throw new SteveException("This is exception method 1");
     }
 
     public void exceptionThrow2() throws SteveException {
-        throw new SteveException("This is com.jets.day4.exception method 2");
+        throw new SteveException("This is exception method 2");
     }
 
     public void exceptionThrow3() throws SteveException {
-        throw new SteveException("This is com.jets.day4.exception method 3");
+        throw new SteveException("This is exception method 3");
     }
 }
